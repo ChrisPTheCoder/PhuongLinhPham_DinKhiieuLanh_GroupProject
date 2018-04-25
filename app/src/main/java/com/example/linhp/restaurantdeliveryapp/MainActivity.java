97f07680eefe0f8458d1fbcfeeb25970de7d54a0
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2018-04-24 23:58:35 -0400

    Fix UI and add Adapter for Image

--- app/src/main/java/com/example/linhp/restaurantdeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/linhp/restaurantdeliveryapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.linhp.restaurantdeliveryapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txtEmail, txtPassword;
+    Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void register(View view)
+    {
+        Intent i = new Intent(this, OrderPageActivity.class);
+        startActivity(i);
     }
 }
